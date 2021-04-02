a224732beeb45b9b7217644c5f5862331ad1ba1d
ArclingML <arcling@Han-Tyumi.localdomain>
2021-04-02 15:31:28 -0700

    pulldown animation implemented; requires manual height update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,22 @@
 var fade_in = anime({
   targets: '.fade-in',
-  translateY: [-20, 0],
+  //translateY: [-20, 0],
   opacity: [0, 1],
-  delay: anime.stagger(300, {start: 500}),
+  delay: anime.stagger(100, {start: 500}),
+  duration: 1000,
+  easing: 'easeOutQuad'
+})
+
+var pull_down = anime({
+  targets: '.pull-down',
+  height: 500,
+  duration: 2000,
+  easing: 'easeOutQuad'
+})
+var pull_down_fadein = anime({
+  targets: '.pull-down',
+  opacity: [0, 1],
+  delay: 300,
   duration: 1000,
   easing: 'easeOutQuad'
 })
