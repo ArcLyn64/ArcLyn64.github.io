999860f62c8ce807e2e4ce4c5a2f60a6df6bc50a
ArclingML <arcling@Han-Tyumi.localdomain>
2021-04-05 13:50:15 -0700

    finished remodel basic

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,15 @@ var fade_in = anime({
   targets: '.fade-in',
   //translateY: [-20, 0],
   opacity: [0, 1],
-  delay: anime.stagger(100, {start: 500}),
+  delay: anime.stagger(50, {start: 500}),
   duration: 1000,
   easing: 'easeOutQuad'
 })
 
 var pull_down = anime({
   targets: '.pull-down',
-  height: 500,
-  duration: 2000,
+  height: 1000,
+  duration: 3000,
   easing: 'easeOutQuad'
 })
 var pull_down_fadein = anime({
